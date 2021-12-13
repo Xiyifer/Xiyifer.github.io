a4d7d87c67710ee9ac2f9ee5df06945e3b6d9a9c
Xiyifer <2420459817@qq.com>
2021-12-13 19:01:44 +0800

    update

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,20 +1,8 @@
 const source = `
 \`\`\`javascript
 const YiyangSun = {
-  introduction: "Hi, there 👋! I'm Yiyang Sun. Currently student of UESTC. Frontend developer.",
-  blog: "https://blog.syy11.cn/",
-  github: "https://github.com/syy11cn/",
-  twitter: "https://zhihu.com/people/syy11cn/",
-  works: [
-    {
-      name: "hexo-theme-linear",
-      repo: "https://github.com/syy11cn/hexo-theme-linear/",
-    },
-    {
-      name: "config-router",
-      repo: "https://github.com/syy11cn/config-router/",
-    },
-  ],
+  introduction: "Hi, there 👋! I'm cn. Currently student of UESTC. back-end developer.",
+  blog: "https://blog.cnijj.cn/"
 };
 \`\`\`
 `;
